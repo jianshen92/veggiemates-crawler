9c9631656859f2fb5eb66333839401d0df3ac47c
jianshen92 <jianshen92@gmail.com>
2021-12-02 10:57:56 +0800

    change caching strategy

--- gcloud_utils.py
+++ gcloud_utils.py
@@ -13,14 +13,12 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     client = storage.Client(credentials=credentials, project='cnft-loots')
     bucket = client.get_bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-
-    blob.metadata = {
-        "Cache-Control" : 'no-store'
-    }
-    blob.patch()
+    blob = bucket.get_blob(destination_blob_name)
 
     blob.upload_from_filename(source_file_name)
+
+    blob.cache_control = "no-cache"
+    blob.patch()
 
     print('File {} uploaded to {}.'.format(
         source_file_name,
